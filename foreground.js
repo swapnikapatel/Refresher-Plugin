c8a02b95c816157045992976df771506b6933cd3
Swapnika Patel <swapnikapatel2016@gmail.com>
2021-06-23 18:32:30 -0400

    Pop up design + function call

--- foreground.js
+++ foreground.js
@@ -1,10 +1,32 @@
 console.log("foreground");
 
-setInterval(function(){
-    document.querySelector("button[name='refreshButton']").click();
-    console.log("Rereshing");
-    // var dt = new Date();
-    // var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-    // append.querySelector()
+// setInterval(function(){
+//     document.querySelector("button[name='refreshButton']").click();
+//     console.log("Rereshing");
+//     // var dt = new Date();
+//     // var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+//     // append.querySelector()
     
-},60000)
+// },60000)
+
+chrome.storage.local.get((value) => {
+    console.log(value);
+
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+
+    console.log(time)
+
+    if (value.func == "start") {
+        localStorage.setItem("intervalId", setInterval(function () {
+            document.querySelector('button[name="refreshButton"]').click();
+        }, parseInt(value.time * 1000)));
+        console.log("start : "+localStorage.getItem("intervalId"))
+    } else if (value.func == "stop") {
+        console.log("stop : "+localStorage.getItem("intervalId"))
+        clearInterval(localStorage.getItem("intervalId"));
+    } else {
+        chrome.storage.local.clear()
+    }
+
+});
--- pop-up.js
+++ pop-up.js
@@ -1,2 +1,36 @@
-chrome.tabs.executeScript(null, { file: './foreground.js' })
+document.addEventListener('DOMContentLoaded', function() {
 
+    var btnstart= document.getElementById("btnstart");
+    var btnstop= document.getElementById("btnstop");
+
+    btnstart.addEventListener('click',fnStart);
+    btnstop.addEventListener('click',fnStop);
+
+
+    function fnStart(){
+
+        var refreshTime = document.getElementById("refreshTime").value;
+
+        chrome.storage.local.clear();
+        chrome.storage.local.set({
+            "func": "start",
+            "time": refreshTime
+        });
+
+        chrome.tabs.executeScript(null, { file: './foreground.js' });
+        console.log("time set")
+    };
+    
+    function fnStop(){
+    
+        chrome.storage.local.clear();
+        chrome.storage.local.set({
+            "func": "stop",
+            "time": "0"
+        });
+
+        chrome.tabs.executeScript(null, { file: './foreground.js' });
+        console.log("time stopped")
+    };
+    
+});
