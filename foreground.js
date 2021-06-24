dd870a98433d8b477904bb432b5af88460c3b81f
Swapnika Patel <swapnikapatel2016@gmail.com>
2021-06-24 17:49:17 -0400

    Append current upadted time

--- foreground.js
+++ foreground.js
@@ -6,24 +6,35 @@ console.log("foreground");
 //     // var dt = new Date();
 //     // var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 //     // append.querySelector()
-    
+
 // },60000)
 
 chrome.storage.local.get((value) => {
     console.log(value);
+    // console.log(time)
 
-    var dt = new Date();
-    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    function displayTime() {
+        $('#refreshTimeCurrent').remove();
+        var dt = new Date();
+        
+        var hours = (dt.getHours() - 12) < 10 ? "0" + (dt.getHours() - 12) : dt.getHours - 12;
+        var minutes = (dt.getMinutes()) < 10 ? ("0" + dt.getMinutes()) : dt.getMinutes();
+        var seconds = (dt.getSeconds()) < 10 ? ("0" + dt.getSeconds()) : dt.getSeconds();
+        var daynight = (dt.getHours() <= 12 ? " AM" : " PM");
 
-    console.log(time)
+        var time = hours + " : " + minutes + " : " + seconds + " " + daynight;
+
+        return "&nbsp;at " + time;
+    }
 
     if (value.func == "start") {
         localStorage.setItem("intervalId", setInterval(function () {
             document.querySelector('button[name="refreshButton"]').click();
+            $('.slds-text-body--small').append("<span id='refreshTimeCurrent'>" + displayTime() + "</span>").css('display', 'inline-flex');
         }, parseInt(value.time * 1000)));
-        console.log("start : "+localStorage.getItem("intervalId"))
+        console.log("start : " + localStorage.getItem("intervalId"))
     } else if (value.func == "stop") {
-        console.log("stop : "+localStorage.getItem("intervalId"))
+        console.log("stop : " + localStorage.getItem("intervalId"))
         clearInterval(localStorage.getItem("intervalId"));
     } else {
         chrome.storage.local.clear()
