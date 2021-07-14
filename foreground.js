119678751d57ee721cc61c9d46f1a8bbfa15ad27
Swapnika Patel <swapnikapatel2016@gmail.com>
2021-07-14 17:57:40 -0400

    Validation fixed + up and running

--- foreground.js
+++ foreground.js
@@ -1,29 +1,13 @@
 console.log("foreground");
 
-// setInterval(function(){
-//     document.querySelector("button[name='refreshButton']").click();
-//     console.log("Rereshing");
-//     // var dt = new Date();
-//     // var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-//     // append.querySelector()
-
-// },60000)
-
-// chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
-//     clearInterval(localStorage.getItem("intervalId"));
-//         $('#refreshTimeCurrent').remove();
-//   });
-
 chrome.storage.local.get((value) => {
-    console.log(value);
-    // console.log(time)
 
     function displayTime() {
         $('#refreshTimeCurrent').remove();
         var dt = new Date();
-        
-        var hours =  (dt.getHours()) <= 12 ?  (dt.getHours() < 10 ? "0" + dt.getHours() : dt.getHours())
-                    : ((dt.getHours() - 12) < 10 ? "0" + (dt.getHours() - 12) :"");
+
+        var hours = (dt.getHours()) <= 12 ? (dt.getHours() < 10 ? "0" + dt.getHours() : dt.getHours())
+            : ((dt.getHours() - 12) < 10 ? "0" + (dt.getHours() - 12) : "");
         var minutes = (dt.getMinutes()) < 10 ? ("0" + dt.getMinutes()) : dt.getMinutes();
         var seconds = (dt.getSeconds()) < 10 ? ("0" + dt.getSeconds()) : dt.getSeconds();
         var daynight = (dt.getHours() <= 12 ? " AM" : " PM");
@@ -34,20 +18,27 @@ chrome.storage.local.get((value) => {
     }
 
     if (value.func == "start") {
-        localStorage.setItem("intervalId", setInterval(function () {
-            document.querySelector('button[name="refreshButton"]').click();
-            // $('.forceListViewManagerHeader p.slds-text-body--small').append("<span id='refreshTimeCurrent'>" + displayTime() + "</span>").css('display', 'inline-flex');
 
-            $("p > force-list-view-manager-status-info").append("<span id='refreshTimeCurrent'>" + displayTime() + "</span>").css('display', 'inline-flex');
-           
-        }, parseInt(value.time * 1000)));
-        console.log("start : " + localStorage.getItem("intervalId"))
+        if (document.getElementById("refreshTimeCurrent") === null) {
+
+            localStorage.setItem("intervalId", setInterval(function () {
+                document.querySelector('button[name="refreshButton"]').click();
+
+                $("p > force-list-view-manager-status-info").append("<span id='refreshTimeCurrent'>" + displayTime() + "</span>").css('display', 'inline-flex');
+                $('.forceRelatedListDesktop:contains("Files")').find('span#refreshTimeCurrent').remove();
+                $('.forceRelatedListDesktop:contains("Case History")').find('span#refreshTimeCurrent').remove();
+
+            }, parseInt(value.time * 1000)));
+
+        }
+
+
     } else if (value.func == "stop") {
-        console.log("stop : " + localStorage.getItem("intervalId"))
         clearInterval(localStorage.getItem("intervalId"));
+        localStorage.clear("intervalId");
         $('#refreshTimeCurrent').remove();
     } else {
-        chrome.storage.local.clear()
+        chrome.storage.local.clear("intervalId")
     }
 
 });
--- pop-up.js
+++ pop-up.js
@@ -1,108 +1,129 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    // chrome.storage.local.get((value) => {
-    //     console.log(value)
-
-    //     // if (parseInt(value.time) > 0) {
-
-    //     //     $("#status").html("")
-    //     //     $("#status").html("In progress every " + value.time + " seconds");
-    //     //     $("#refreshTime").val("")
-    //     //     $("#btnstart").hide();
-    //     //     $("#btnstop").show();
-    //     //     // fnStart();
-
-    //     // }
-
-    //     fnStop();
-    // })
-
     var btnstart = document.getElementById("btnstart");
     var btnstop = document.getElementById("btnstop");
 
-    $("#negativeNumber").hide();
-    $("#btnstop").hide();
-
-
-    document.getElementById("refreshTime").addEventListener('input', btnActivity);
+    document.getElementById("refreshTime").addEventListener('input', timeValue);
     btnstart.addEventListener('click', fnStart);
     btnstop.addEventListener('click', fnStop);
 
-    function btnActivity(e) {
+    var localChromeStorage = checkIfLocalStorage();
 
-        console.log(e.target.value)
-
-        parseInt(e.target.value) > 0 ? $("#btnstart").removeAttr("disabled") : $("#btnstart").attr("disabled", "disabled");
-
+    if (localChromeStorage.storage === false) {
+        $("#negativeNumber").hide();
+        $("#btnstop").hide();
+    } else if (localChromeStorage.storage === true) {
+        $("#status").html("")
+        $("#status").html("In progress every " + JSON.parse(localChromeStorage.current).time + " seconds");
+        $("#refreshTime").val("")
+        $("#btnstart").hide();
+        $("#btnstop").show();
+        $("#refreshTime").attr("disabled", "disabled");
+        refreshStart();
     }
 
-    function fnStart() {
+})
 
-        // var refreshTime;
-        var refreshTime = document.getElementById("refreshTime").value;
+function checkIfLocalStorage() {
 
-        console.log("fnstart called")
+    var current = localStorage.getItem("currentFunction");
+    if (current === null) {
+        return { storage: false }
+    } else {
+        return { storage: true, current }
+    }
 
-        // chrome.storage.local.get((value) => {
-        //     console.log("Test ")
-        //     console.log(value)
-        //     if(value.time == undefined){
-        //         $('#negativeNumber').html("Time not found");
-        //     }
-        //     else if (parseInt(value.time) > 0) {
-        //         refreshTime = value.time;
-        //     } else {
-        //         refreshTime = document.getElementById("refreshTime").value;
-        //     }
-        // })
+}
 
-        console.log("fnstart called :"+refreshTime)
+function timeValue(e) {
+    parseInt(e.target.value) > 0 ? (
+        $("#btnstart").removeAttr("disabled"),
+        $("#negativeNumber").is(":visible") ?
+            ($("#negativeNumber").html(""),
+                $("#negativeNumber").hide()) : ""
+    )
+        : ($("#btnstart").attr("disabled", "disabled"),
+            $("#negativeNumber").show(),
+            $('#negativeNumber').html("Enter number greater than 0!"));
 
-        if (refreshTime == "" || null) {
+}
 
-        }
+function fnStart() {
 
-        else if (refreshTime <= 0) {
-            $("#negativeNumber").show();
-            $('#negativeNumber').html("Enter number greater than 0");
-        }
-        else {
-            chrome.storage.local.clear();
-            chrome.storage.local.set({
-                "func": "start",
-                "time": refreshTime
-            });
+    chrome.runtime.sendMessage({ greeting: "hello" });
 
-            chrome.tabs.executeScript(null, { file: './foreground.js' });
-            console.log("time set");
+    var currentFunction;
+    var refreshTime = document.getElementById("refreshTime").value;
 
-            $("#status").html("")
-            $("#status").html("In progress every " + refreshTime + " seconds");
-            $("#refreshTime").val("")
-            $("#btnstart").hide();
-            $("#negativeNumber").hide();
-            $("#btnstop").show();
-        }
+    if (refreshTime === undefined || "" || null) {
+        $('#negativeNumber').html("Time not found");
+    } else if (parseInt(refreshTime) <= 0) {
+        $("#negativeNumber").show();
+        $('#negativeNumber').html("Enter number greater than 0!");
+    } else {
 
-    };
+        currentFunction = JSON.stringify({
+            func: "start",
+            time: refreshTime
+        });
 
-    function fnStop() {
+        localStorage.setItem("currentFunction", currentFunction);
 
         chrome.storage.local.clear();
         chrome.storage.local.set({
-            "func": "stop",
-            "time": "0"
+            "func": "start",
+            "time": refreshTime
         });
 
         chrome.tabs.executeScript(null, { file: './foreground.js' });
-        console.log("time stopped")
 
         $("#status").html("")
-        $("#status").html("Ready");
-        $("#btnstop").hide();
-        $("#btnstart").show();
-        $("#btnstart").attr("disabled", "disabled");
-    };
+        $("#status").html("In progress every " + refreshTime + " seconds");
+        $("#refreshTime").val("")
+        $("#btnstart").hide();
+        $("#negativeNumber").hide();
+        $("#btnstop").show();
+        $("#refreshTime").attr("disabled", "disabled");
+    }
 
+};
 
-});
+function refreshStart() {
+
+    var refreshTime;
+    var localChromeStorage = checkIfLocalStorage();
+    refreshTime = JSON.parse(localChromeStorage.current).time;
+
+    if (refreshTime === undefined || "" || null) {
+        $('#negativeNumber').html("Time not found");
+    } else {
+
+        chrome.storage.local.clear();
+        chrome.storage.local.set({
+            "func": "start",
+            "time": refreshTime
+        });
+
+        chrome.tabs.executeScript(null, { file: './foreground.js' });
+    }
+
+};
+
+function fnStop() {
+
+    localStorage.clear("intervalId")
+    chrome.storage.local.clear();
+    chrome.storage.local.set({
+        "func": "stop",
+        "time": "0"
+    });
+
+    chrome.tabs.executeScript(null, { file: './foreground.js' });
+
+    $("#status").html("")
+    $("#status").html("Ready");
+    $("#btnstop").hide();
+    $("#btnstart").show();
+    $("#btnstart").attr("disabled", "disabled");
+    $("#refreshTime").removeAttr("disabled");
+};
